3457a8b40ec68f7abf508d704aebadc5fc68f846
unknown <Sagarika517>
2025-01-04 16:25:51 +0530

    login 4 functionality

--- src/LoginFunctionality/loginwithWUWOP.java
+++ src/LoginFunctionality/loginwithWUWOP.java
@@ -3,20 +3,19 @@ package LoginFunctionality;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginwithInvalidcredentials {
+public class loginwithWUWOP {
 
 	public static void main(String[] args) {
-		
 		// TODO Auto-generated method stub
 		String path=System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver","D:\\chromedriver-win64 (3)\\chromedriver-win64\\chromedriver.exe");
+		System.getProperty("webdriver.chrome.driver","D:\\chromedriver-win64 (3)\\chromedriver-win64\\chromedriver.exe");
 		ChromeDriver driver=new ChromeDriver();
 		driver.get("http://127.0.0.1/orangehrm-3.0.1/symfony/web/index.php/auth/login");
 		driver.findElement(By.id("txtUsername")).sendKeys("sagarika");
-		driver.findElement(By.id("txtPassword")).sendKeys("sagarika1234");
+		driver.findElement(By.id("txtPassword")).sendKeys("");
 		driver.findElement(By.id("btnLogin")).click();
 		String actualmessage=driver.findElement(By.id("spanMessage")).getText();
-		String expectedmessage="invalid credentials";
+		String expectedmessage="password cannot empty";
 		if(actualmessage.equals(expectedmessage))
 		{
 			System.out.println("test pass");
@@ -25,8 +24,6 @@ public class LoginwithInvalidcredentials {
 		{
 			System.out.println("test fail");
 		}
-		
-
 	}
 
 }
